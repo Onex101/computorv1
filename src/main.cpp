ec4379326fbc40e750717930cd04a4c02a67c9a3
Xeno RHODA <xrhoda@c1r2s2.wethinkcode.co.za>
2019-11-04 09:19:28 +0200

    Think its don

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,38 @@
 #include <sstream>
 
 bool isOperation(std::string term, char &operation){
-    if (term == "+"){
+    char tmp = term.at(0);
+    if (tmp == '+'){
         operation = '+';
         return true;
     }
-    else if (term == "-"){
+    else if (tmp == '-'){
         operation = '-';
         return true;
     }
-    else if (term == "*"){
+    else if (tmp == '*'){
         operation = '*';
         return true;
     }
-    else if (term == "="){
+    else if (tmp == '='){
         operation = '=';
+        return true;
+    }
+    operation = '+';
+    return false;
+}
+
+bool isOperationBool(std::string term){
+    if (term == "+"){
+        return true;
+    }
+    else if (term == "-"){
+        return true;
+    }
+    else if (term == "*"){
+        return true;
+    }
+    else if (term == "="){
         return true;
     }
     return false;
@@ -43,13 +61,20 @@ int checkDegree(std::string term){
     return 3;
 }
 
-float ft_strtof(std::string s, int degree)
+float ft_strtof(std::string s)
 {
     float number;
     std::stringstream ss;
     ss << s;
     ss >> number;
     return number;
+}
+
+std::string ft_ftostr(float myFloat){
+    std::ostringstream ss;
+    ss << myFloat;
+    std::string s(ss.str());
+    return s;
 }
 
 float sub(float i, float j){
@@ -69,84 +94,86 @@ int main (int argc, char **argv) {
     std::vector<std::string> terms;
     std::string operations;
     std::stringstream line;
-    float reduced[4] = {1 , 1 , 1 , 0};
+    float reduced[4] = {0 , 0 , 0 , 0};
     int degree;
     char coefficent;
     bool swap;
+    int swapNow;
     char * pch;
     char operation;
     
     if (*argv){
         std::vector<std::string> terms;
         std::string term;
-        // std::istringstream termStream(argv[0]);
-        // while (std::getline(termStream, term, ' '))
-        // {
-        //     terms.push_back(term);
-        // }
         std::string str = argv[0];
         std::string::size_type pos = std::string::npos;
         std::string prevOperation = "";
-        while ((pos = str.find_first_of("=+-")) != std::string::npos) {
+        int swapStage = 0;
+        std::cout << str + "\n";
+        while ((pos = str.find_first_of("+-=")) != std::string::npos) {
+            if (prevOperation == ""){
+                prevOperation = str.at(0);
+                if (!isOperationBool(prevOperation))
+                    prevOperation = "";
+            }
             std::string str2 = str.substr(0, pos);
             std::string chars = "* ";
             for (char c: chars) {
                 str2.erase(std::remove(str2.begin(), str2.end(), c), str2.end());
             }
-            prevOperation = str.at(pos);
             if (str2.length() > 0){
-                terms.push_back(str2);
+                terms.push_back(prevOperation + str2);
             }
+            prevOperation = str.at(pos);
+            if (prevOperation == "="){
+                prevOperation = "";
+                swapNow = swapStage;
+            }
+            swapStage++;
             operations += (str.at(pos));
-            std::cout << prevOperation << "\n";
             str.erase(0, pos + 1);
         }
         std::string chars = "* ";
         for (char c: chars) {
             str.erase(std::remove(str.begin(), str.end(), c), str.end());
         }
-        terms.push_back(str);
+        terms.push_back(prevOperation + str);
         for(int i=0; i < terms.size(); i++){
-            std::cout << terms.at(i);
-            if (isOperation(terms.at(i), operation)){
-                // std::cout << operation;
-                continue;
-            }
-            else if (terms.at(i) == "="){
+            // std::cout << terms.at(i);
+            isOperation(terms.at(i), operation);
+            if (terms.at(i) == "="){
                 swap = false;
             }
             else{
                 degree = checkDegree(terms.at(i));
-                std::cout << "| Term " << ft_strtof(terms.at(i), degree);
-                std::cout << "| Operation: " << operation;
-                std::cout << "| Degree: " << degree <<"\n";
-                float value = ft_strtof(terms.at(i), degree);
-                if (operation == '=') {
-                    if (swap)
+                int pos = terms.at(i).find_first_of("X");
+                float value = ft_strtof(terms.at(i).substr(0, pos));
+                if (i < swapNow)
                         reduced[degree] += value;
                     else
                         reduced[degree] -= value;
-                }
-                else if (operation == '-'){
-                    if (swap)
-                        reduced[degree] -= - value;
-                    else
-                        reduced[degree] += value;
-                }    
-                else if (operation == '+'){
-                    if (swap)
-                        reduced[degree] += value;
-                    else
-                        reduced[degree] -= value;
-                }
             }
-            // std::cout << degree;
         }
-        std::cout << "\nNew Values: ";
-        for (int i = 0; i < 4; i++)
+
+        for (int i = 0; i < 3; i++)
         {
-            std::cout << " " << reduced[i];
+            std::string neg = ft_ftostr(reduced[i] * -1) + " * X^" + ft_ftostr(i);
+            std::string pos = ft_ftostr(reduced[i]) + " * X^" + ft_ftostr(i);
+            if (i == 0){
+                if (reduced[i] < 0)
+                    std::cout << "- " + neg;
+                else
+                    std::cout << pos;
+            }
+            else{
+                if (reduced[i] < 0)
+                    std::cout << " - " + neg;
+                else
+                    std::cout << " + " + pos;
+            }
         }
+
+        std::cout << " = 0\n";
     }
     return 0; 
 }
